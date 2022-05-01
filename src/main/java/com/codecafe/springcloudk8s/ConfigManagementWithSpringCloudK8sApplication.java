a94e6c554757829bd47e1fad0aa408612094f17a
Abhinav Nath <abhinavnath@ymail.com>
2022-05-01 13:49:41 +0530

    Initialise project

---
+++ src/main/java/com/codecafe/springcloudk8s/ConfigManagementWithSpringCloudK8sApplication.java
@@ -0,0 +1,13 @@
+package com.codecafe.springcloudk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigManagementWithSpringCloudK8sApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigManagementWithSpringCloudK8sApplication.class, args);
+  }
+
+}
